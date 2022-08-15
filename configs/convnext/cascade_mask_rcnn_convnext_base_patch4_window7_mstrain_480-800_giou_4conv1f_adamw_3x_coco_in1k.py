20708c5350648ddce2671909eff376de636e7adc
Chris Hughes <31883449+Chris-hughes10@users.noreply.github.com>
2022-08-15 11:52:43 +0100

    update configs

--- configs/convnext/cascade_mask_rcnn_convnext_base_patch4_window7_mstrain_480-800_giou_4conv1f_adamw_3x_coco_in1k.py
+++ configs/convnext/cascade_mask_rcnn_convnext_base_patch4_window7_mstrain_480-800_giou_4conv1f_adamw_3x_coco_in1k.py
@@ -5,10 +5,8 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
-
 _base_ = [
     '../_base_/models/cascade_mask_rcnn_convnext_fpn.py',
-    '../_base_/datasets/coco_instance.py',
     '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
 ]
 
@@ -89,7 +87,7 @@ img_norm_cfg = dict(
 # augmentation strategy originates from DETR / Sparse RCNN
 train_pipeline = [
     dict(type='LoadImageFromFile'),
-    dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
+    dict(type='LoadAnnotations', with_bbox=True, with_mask=False),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='AutoAugment',
          policies=[
@@ -123,9 +121,53 @@ train_pipeline = [
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks']),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+
 ]
-data = dict(train=dict(pipeline=train_pipeline))
+
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(1333, 800),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ]) ]
+
+dataset_type = 'CocoDataset'
+# data_root = 'data/coco/'
+
+
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=2,
+    train=dict(
+        type=dataset_type,
+        ann_file='C:/Users/hughesc/Documents/data/coco/annotations_trainval2017/annotations/instances_val2017.json',
+        # ann_file=data_root + 'annotations/instances_train2017.json',
+        # img_prefix=data_root + 'train2017/',
+        img_prefix='C:/Users/hughesc/Documents/data/coco/val2017',
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        # ann_file=data_root + 'annotations/instances_val2017.json',
+        ann_file='C:/Users/hughesc/Documents/data/coco/annotations_trainval2017/annotations/instances_val2017.json',
+        img_prefix='C:/Users/hughesc/Documents/data/coco/val2017',
+        pipeline=test_pipeline),
+    # test=dict(
+    #     type=dataset_type,
+    #     ann_file=data_root + r'annotations_trainval2017\annotations\instances_val2017.json',
+    #     # ann_file=data_root + 'annotations/instances_val2017.json',
+    #     img_prefix=data_root + 'val2017/',
+    #     pipeline=test_pipeline)
+        )
+evaluation = dict(interval=1, metric='bbox')
 
 optimizer = dict(constructor='LearningRateDecayOptimizerConstructor', _delete_=True, type='AdamW', 
                  lr=0.0002, betas=(0.9, 0.999), weight_decay=0.05,
